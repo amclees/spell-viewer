ff713ca27c119e7796ff332db9ce61c8fdbce154
amclees <andrewmclees5@gmail.com>
2017-06-10 17:14:23 -0700

    Add basic view component

--- src/app/view.component.ts
+++ src/app/view.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Spell } from './spell';
 
 @Component({
   selector: 'spell-view',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class ViewComponent {
-  spell: Object
+  @Input() spell: Spell;
+
+  format(level: number, school: string): string {
+    if (level === 0) {
+      return `${school} cantrip`;
+    }
+    let suffix = 'th';
+    switch (level % 10) {
+      case 1:
+        suffix = 'st';
+        break;
+      case 2:
+        suffix = 'nd';
+        break;
+      case 3:
+        suffix = 'rd';
+        break;
+    }
+    return `${level}${suffix}-level ${school.toLowerCase()}`;
+  }
 }
