0bab81649ba91524956f20b955ffd10a0ab4ecc5
amclees <andrewmclees5@gmail.com>
2017-06-15 10:39:12 -0700

    Scroll to top of page on route change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+  constructor(private router: Router) {
+    this.router.events.subscribe((path) => {
+      window.scrollTo(0, 0);
+    });
+  }
+}
