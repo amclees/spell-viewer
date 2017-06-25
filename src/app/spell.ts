e7f62024042f4c7fc5d29cda705fee4e17003ca5
amclees <andrewmclees5@gmail.com>
2017-06-25 13:30:23 -0700

    Add additional checks to spell constructor

--- src/app/spell.ts
+++ src/app/spell.ts
@@ -15,6 +15,22 @@ export class Spell {
     public ritual: boolean,
     public concentration: boolean
   ) {
+    if (
+      name === undefined 
+      || castingTime == undefined 
+      || components === undefined 
+      || description === undefined 
+      || duration === undefined
+      || level === undefined
+      || range === undefined
+      || school === undefined
+      || classes === undefined
+      || urlName === undefined
+      || ritual === undefined
+      || concentration === undefined
+    ) {
+      throw new Error('Invalid Spell: Not all fields defined');
+    }
     if (level < 0 || level > 9) {
       throw new Error('Invalid Spell: Level out of range');
     }
